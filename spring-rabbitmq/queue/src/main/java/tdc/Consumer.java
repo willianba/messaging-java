4f87274771aa6a1cac52be42db2827c9deccc11f
Willian Alves <wba.alves@gmail.com>
2019-11-16 20:49:46 -0300

    create spring-boot with rabbitmq app

---
+++ spring-rabbitmq/queue/src/main/java/tdc/Consumer.java
@@ -0,0 +1,18 @@
+package tdc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = {"${queue.name}"})
+public class Consumer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+
+  @RabbitHandler
+  public void receiveMessage(String value) {
+    LOGGER.info("Value: " + value);
+  }
+}
